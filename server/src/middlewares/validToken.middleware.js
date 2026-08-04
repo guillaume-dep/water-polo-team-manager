7711cae51602ccd5c2f3f21f8fecfc745daddce4
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-04 19:35:29 +0200

    feat: validToken verification

---
+++ server/src/middlewares/validToken.middleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+
+const validToken = (req, res, next) => {
+    const token = req.cookies.token
+
+    if (!token){
+        return res.status(401).json({message: "Unauthentified"})
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded /* decoded = { id: "...", role: "coach", iat: ..., exp: ... } */
+        next()
+    }
+    catch(err){
+        return res.status(401).json({ message: "Invalid or expired token" });
+    }
+}
