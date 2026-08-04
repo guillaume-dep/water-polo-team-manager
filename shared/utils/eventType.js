45cd3f90aed857a6fa3574172ad460fdd6791cf2
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-04 14:13:10 +0200

    feat: enum types

---
+++ shared/utils/eventType.js
@@ -0,0 +1,7 @@
+const EVENT_TYPE = {
+    MATCH: "match",
+    TRAINING: "training",
+    OTHER: "other"
+}
+
+export default EVENT_TYPE;
---
+++ shared/utils/responseType.js
@@ -0,0 +1,7 @@
+const RESPONSE_TYPE = {
+    PRESENT: "present",
+    ABSENT: "absent",
+    UNCERTAIN: "uncertain"
+}
+
+export default RESPONSE_TYPE;
