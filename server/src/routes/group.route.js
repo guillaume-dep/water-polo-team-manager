7697a14ad6d6c5df7b58d10e5fe33cb4827724a5
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-05 12:50:59 +0200

    feat: implementation of group route and requireRole

--- server/src/routes/group.route.js
+++ server/src/routes/group.route.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import validToken from '../middlewares/validToken.middleware.js'
+import requireRole from '../middlewares/requireRole.middleware.js'
+import ROLE from '../../../shared/utils/role.js'
 const router = express.Router()
+
+router.post('/', validToken, requireRole(ROLE.COACH), createGroup)
+router.post('/join', validToken, requireRole(ROLE.COACH), joinGroup)
+router.post('/my', validToken, getMyGroups)
 
 export default router
