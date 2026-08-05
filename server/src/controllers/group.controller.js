afc9895c72fb6fea9145137eff85f5ca93ab18b4
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-05 13:39:59 +0200

    feat: group routes

---
+++ server/src/controllers/group.controller.js
@@ -0,0 +1,19 @@
+export const searchGroupByCode = async(res, req) => {
+
+}
+
+export const createGroup = async(req, res) => {
+
+}
+
+export const joinGroup = async(req, res) => {
+
+}
+
+export const getMyGroup = async(req, res) => {
+
+}
+
+export const leaveGroup = async(req, res) => {
+
+}
--- server/src/routes/group.route.js
+++ server/src/routes/group.route.js
@@ -2,10 +2,20 @@ import express from 'express'
 import validToken from '../middlewares/validToken.middleware.js'
 import requireRole from '../middlewares/requireRole.middleware.js'
 import ROLE from '../../../shared/utils/role.js'
+import {searchGroupByCode, createGroup, joinGroup, getMyGroups, leaveGroup} from '../controllers/group.controller.js'
+
 const router = express.Router()
 
+router.get('/', validToken, searchGroupByCode)
 router.post('/', validToken, requireRole(ROLE.COACH), createGroup)
-router.post('/join', validToken, requireRole(ROLE.COACH), joinGroup)
-router.post('/my', validToken, getMyGroups)
+
+/* Identifies a user instead of filtering in the controller */
+router.get('/me', validToken, getMyGroups)
+router.delete('/:id', validToken, requireRole(ROLE.COACH), deleteGroup)
+
+router.post('/:id/members', validToken, requireRole(ROLE.PLAYER), joinGroup)
+
+/* Delete myself from the list "members" of the group ":id" */
+router.delete('/:id/members/me', validToken, requireRole(ROLE.PLAYER), leaveGroup)
 
 export default router
