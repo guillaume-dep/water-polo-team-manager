f9472c39fee81fe716ef9320c87a9d859aff58eb
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-05 13:53:20 +0200

    feat: searchGroupByCode

--- server/src/controllers/group.controller.js
+++ server/src/controllers/group.controller.js
@@ -1,5 +1,20 @@
-export const searchGroupByCode = async(res, req) => {
+import Groups from "../models/group.model"
 
+export const searchGroupByCode = async(res, req) => {
+    try {
+        const {code} = req.query
+        if (!code) {
+            return res.status(400).json({message: "Missing code"})
+        }
+        const group = await Groups.findOne({code});
+        if (!group){
+            return res.status(404).json({message: "Group not found"})
+        }
+        res.json(group)
+    }
+    catch(err){
+        res.status(500).json({ message: err.message });
+    }
 }
 
 export const createGroup = async(req, res) => {
@@ -7,7 +22,12 @@ export const createGroup = async(req, res) => {
 }
 
 export const joinGroup = async(req, res) => {
+    try {
 
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
 }
 
 export const getMyGroup = async(req, res) => {
