f43f9eac7a3a345d50de6818612a84c01ba45b41
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-07 19:34:57 +0200

    feat: comments

--- server/src/utils/dbFinder.js
+++ server/src/utils/dbFinder.js
@@ -1,6 +1,8 @@
 import Events from "../models/event.model.js";
 import Groups from "../models/group.model.js";
 
+/* Should use OOP and create an Object FindInCollectionById(collection, objectId) */
+
 export const findGroupById = async (groupId) => {
     const group = await Groups.findById(groupId);
     if (!group) throw new Error("Group not found");
