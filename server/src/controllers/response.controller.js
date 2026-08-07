8066e58a328de64d3c915e0a762fcce980c6f9e7
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-07 20:37:29 +0200

    feat: adding getResponses and create

--- server/src/controllers/response.controller.js
+++ server/src/controllers/response.controller.js
@@ -1,13 +1,29 @@
-import RESPONSE_TYPE from "../../../shared/utils/responseType.js"
-import Events from "../models/event.model.js"
 import Responses from "../models/response.model.js"
+import { findEventById, findGroupById } from "../utils/dbFinder.js"
+import { checkIsMember, checkIsMemberOrCoach } from "../utils/logicChecker.js"
 
 export const createResponse = async(req, res) => {
     try{
-        const eventId = 
+        
+        const group = await findGroupById(req.params.id)
+        checkIsMember(group, req.user.id)
+
+        const event = await findEventById(req.params.eventId)
+        const status = req.body.status
+        const comment = req.body.comment
+        const response = await Responses.create({
+            status,
+            user: req.user.id,
+            event,
+            comment
+        })
+
+        res.status(201).json(response)
+
     }
+
     catch(err){
-        res.status(500).json({message: err.message})
+        res.status(err.status || 500).json({message: err.message})
     }
 }
 
@@ -15,16 +31,23 @@ export const updateResponse = async(req, res) => {
     try{
         
     }
+
     catch(err){
-        res.status(500).json({message: err.message})
+        res.status(err.status || 500).json({message: err.message})
     }
 }
 
 export const getResponses = async(req, res) => {
     try{
-        
+        const group = await findGroupById(req.params.id)
+        checkIsMemberOrCoach(group, req.user.id)
+
+        const event = await findEventById(req.params.eventId)
+        const responses = await Responses.find({event: req.params.eventId})
+
+        res.json(responses)
     }
     catch(err){
-        res.status(500).json({message: err.message})
+        res.status(err.status || 500).json({message: err.message})
     }
 }
--- server/src/routes/response.route.js
+++ server/src/routes/response.route.js
@@ -2,6 +2,7 @@ import express from "express"
 import validToken from "../middlewares/validToken.middleware.js"
 import requireRole from "../middlewares/requireRole.middleware.js"
 import ROLE from "../../../shared/utils/role.js"
+import { createResponse, updateResponse, getResponses } from "../controllers/response.controller.js"
 
 const router = express.Router({mergeParams: true})
 
