84c4f15055feaf60fc395801ff18e599bb2e4783
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-07 19:51:48 +0200

    feat: adding AppError to have custom error

--- server/src/utils/dbFinder.js
+++ server/src/utils/dbFinder.js
@@ -1,16 +1,17 @@
 import Events from "../models/event.model.js";
 import Groups from "../models/group.model.js";
+import AppError from "./AppError.js";
 
 /* Should use OOP and create an Object FindInCollectionById(collection, objectId) */
 
 export const findGroupById = async (groupId) => {
     const group = await Groups.findById(groupId);
-    if (!group) throw new Error("Group not found");
+    if (!group) throw new AppError("Group not found", 404);
     return group;
 };
 
 export const findEventById = async (eventId) => {
     const event = await Events.findById(eventId);
-    if (!event) throw new Error("Event not found");
+    if (!event) throw new AppError("Event not found", 404);
     return event;
 };
--- server/src/utils/logicChecker.js
+++ server/src/utils/logicChecker.js
@@ -1,5 +1,13 @@
+import AppError from "./AppError.js"
+
+export const checkIsInGroup = (group, userId) => {
+    if (!group.members.includes(userId)) {
+        throw new AppError("Not your group", 403);
+    }
+};
+
 export const checkIsCoach = (group, userId) => {
     if (group.coach.toString() !== userId) {
-        throw new Error("Not your group")
+        throw new AppError("You are not a coach", 403)
     }
 }
