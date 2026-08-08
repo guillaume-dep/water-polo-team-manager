9ae27c08ada72a99f779797be8127cd3739f32a4
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-08 18:24:01 +0200

    feat : adding cascade suppression

--- server/src/models/group.model.js
+++ server/src/models/group.model.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import dbConnection from "../config/database.js";
+import Events from "./event.model.js";
+import Responses from "./response.model.js";
 
 /**
  * A group is a bunch of User
@@ -11,18 +13,22 @@ const groupSchema = new mongoose.Schema({
     members: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 }, {timestamps: true});
 
+/* Executed before deleting */
+groupSchema.pre('findOneAndDelete', async function(next) {
+    try{
+        const groupId = this.getQuery()._id /* this = current request ; return the filter */
+
+        const events = await Events.find({group: groupId})
+        const eventIds = events.map(e => e._id) /* own id */
+        await Responses.deleteMany({event : {$in : eventIds}}) /* delete events which are $in eventsIds */
+        await Events.deleteMany({group: groupId})
+        next()
+    }
+    catch(err){
+        next(err)
+    }
+})
+
 const Groups = dbConnection.model('Group', groupSchema)
 
 export default Groups;
-
-/* TODO : AJOUTER CASCADE SUPP : 
-
-groupSchema.pre('findOneAndDelete', async function() {
-    const groupId = this.getQuery()._id
-    const events = await Event.find({ group: groupId })
-    const eventIds = events.map(e => e._id)
-    await Response.deleteMany({ event: { $in: eventIds } })
-    await Event.deleteMany({ group: groupId })
-})
-
-*/
