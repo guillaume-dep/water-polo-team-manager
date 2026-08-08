f82a1ea2d0925eb91c302be195949c54ce905b08
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-08 15:38:00 +0200

    feat: test on auth

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {register, login, logout, /*deleteAccount*/ } from '../controllers/auth.controller.js';
+import validToken from '../middlewares/validToken.middleware.js';
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.post('/logout', logout)
+router.post('/logout', validToken, logout)
 /* router.delete('/delete', deleteAccount) */
 
 export default router;
