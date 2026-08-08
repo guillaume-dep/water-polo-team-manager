b1c3701bb7a0b76ea24301878e45e506ea795241
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-08 22:05:09 +0200

    feat: length control of user

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -6,8 +6,8 @@ import ROLE from "../../../shared/utils/role.js";
  * A user can either be a coach or a player
  */
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    name: {type: String, required: true, trim: true, minlength: 2, maxlength: 30},
+    email: {type: String, required: true, unique: true, trim: true, maxlength: 30},
     passwordHash: {type: String, required: true},
     role: {type: String, enum: Object.values(ROLE), required: true} // list required for enum
 }, {timestamps: true});
