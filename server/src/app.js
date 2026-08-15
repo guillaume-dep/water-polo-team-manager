4935f5b4e123b2967363252507da598c2726b410
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-15 18:43:53 +0200

    feat: login/register are now REALLY working

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dbConnection from './config/database.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 import helmet from 'helmet'
 import rateLimit from 'express-rate-limit'
 import { limiter, authLimiter } from './utils/limiter.js';
@@ -12,6 +13,22 @@ import eventRouter from './routes/event.route.js'
 import responseRouter from './routes/response.route.js'
 
 const app = express()
+const allowedOrigins = [
+    process.env.CLIENT_URL,
+    'http://localhost:5173'
+]
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true
+}))
+console.log(JSON.stringify(process.env.CLIENT_URL))
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 // app.use(limiter)
@@ -19,7 +36,7 @@ app.use(helmet())
 
 /* --- Router + Middlewares --- */
 
-app.use('/auth', authLimiter)
+//app.use('/auth', authLimiter)
 app.use('/auth', authRouter)
 app.use('/groups', groupRouter)
 app.use('/groups/:id/events', eventRouter)
