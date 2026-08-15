4eef818ba5b037811824e2c5ef906eeee6a72ee1
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-15 11:39:17 +0200

    feat: adding api files

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,3 +1,10 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import { getMe } from '../api/auth.js'
+
+const AuthContext = createContext()
+
 export const AuthProvider = () => {
   return <div></div>
 }
+
+export default AuthProvider
