a8e00e339d4868cbf18766e89433ba22ac2fa8f1
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-16 18:50:37 +0200

    feat: routes and profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Test from './pages/Test'
+
 import Register from './pages/auth/Register.jsx'
 import Login from './pages/auth/Login.jsx'
 import Home from './pages/home/Home.jsx'
+import Groups from './pages/groups/Groups.jsx'
+import Events from './pages/events/Events.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
+
 import { useAuth } from './context/hooks/useAuth.js'
 
 const App = () => {
@@ -24,6 +28,33 @@ const App = () => {
         element={
           <ProtectedRoute>
             <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Groups />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Events />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Events />
           </ProtectedRoute>
         }
       />
