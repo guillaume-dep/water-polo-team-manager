fd8558d59231247a9269f6bfe27527f1e0e1893a
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-19 16:57:08 +0200

    feat: list of requesting persons

--- client/src/pages/groups/PendingRequests.jsx
+++ client/src/pages/groups/PendingRequests.jsx
@@ -1,7 +1,52 @@
+import { useParams } from 'react-router-dom'
+import { getJoinRequests } from '../../api/groups.js'
+import { useEffect, useState } from 'react'
+import PersonCard from '../../components/PersonCard.jsx'
+
+import styles from '../../styles/groups/pendingRequests.module.css'
+
 const PendingRequests = () => {
+    const { id: groupId } = useParams()
+
+    const [personRequests, setPersonRequests] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchPersonRequests = async () => {
+            try {
+                const data = await getJoinRequests(groupId)
+                setPersonRequests(data)
+            } catch (err) {
+                console.error(
+                    'Error occured while retrieving persons requests',
+                    err
+                )
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchPersonRequests()
+    }, [groupId])
+
+    if (isLoading) {
+        return <p>Chargement des demandes...</p>
+    }
+
+    if (personRequests.length === 0) {
+        return <p>Aucune demande en attente.</p>
+    }
 
     return (
-        <div>Pending requests</div>
+
+        <div className={styles.container}>
+            {personRequests.map((person) => (
+                <PersonCard
+                    key={person._id}
+                    person={person}
+                />
+            ))}
+        </div>
     )
 }
 
