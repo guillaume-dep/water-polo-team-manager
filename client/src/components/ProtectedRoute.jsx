9e8ab75f318ffc337ee0651a695de6dd9aeabfb1
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-20 21:35:07 +0200

    feat: css loader

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,17 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../context/hooks/useAuth.js'
+import styles from '../styles/protectedRoute.module.css' // Crée ce fichier CSS
 
 const ProtectedRoute = () => {
     const { user, loading } = useAuth()
 
     if (loading) {
-        return <p>Chargement...</p>
+        return (
+            <div className={styles.loaderContainer}>
+                <div className={styles.spinner}></div>
+                <p className={styles.loadingText}>Chargement...</p>
+            </div>
+        )
     }
 
     if (!user) {
