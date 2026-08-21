ae7e6b60cd12f2a94a779978620b0718e4e166fb
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-21 21:50:24 +0200

    feat: limiter

--- server/src/app.js
+++ server/src/app.js
@@ -32,7 +32,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
-app.use(limiter)
+//app.use(limiter)
 app.use(helmet())
 
 /* --- Router + Middlewares --- */
--- server/src/utils/limiter.js
+++ server/src/utils/limiter.js
@@ -1,13 +1,15 @@
 import rateLimit from "express-rate-limit"
 
+const isDev = process.env.NODE_ENV !== 'production'
+
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15'
-    max: 100,
+    windowMs: 15 * 60 * 1000,
+    max: isDev ? 1000 : 100,
     message: { message: "Too many requests, please try again later" }
 })
 
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10,
+    max: isDev ? 100 : 10,
     message: { message: "Too many attempts, please try again later" }
 })
