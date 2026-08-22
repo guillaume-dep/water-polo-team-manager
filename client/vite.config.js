c691e922b7fdff01500e1db0a21632f55e43607d
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-22 10:35:32 +0200

    feat: pwa, manifest, icon

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import manifest from './manifest.json'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: manifest,
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => {
+              return ['/auth', '/groups'].some(prefix => url.pathname.startsWith(prefix))
+            },
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              networkTimeoutSeconds: 5,
+              cacheableResponse: { statuses: [0, 200] }
+            }
+          }
+        ]
+      }
+    })
+  ],
 })
+
+/* 
+urlPattern: ({ url }) => url.origin === import.meta.env.VITE_API_URL
+*/
