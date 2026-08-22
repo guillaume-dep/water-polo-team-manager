8dea80dede89e55933a0e7b3df2b4f158e961d10
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-22 18:29:26 +0200

    feat: production mode

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -9,22 +9,11 @@ export const generateToken = (user) => {
     return token
 };
 
-/*
 export const setTokenCookie = (res, token) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-}
-*/
-
-export const setTokenCookie = (res, token) => {
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 }
