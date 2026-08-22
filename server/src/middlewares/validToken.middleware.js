0147aa3f9d9fb54fe8b62cd12941057b90d3c11b
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-22 18:13:02 +0200

    fix: validToken sending res himself

--- server/src/middlewares/validToken.middleware.js
+++ server/src/middlewares/validToken.middleware.js
@@ -1,27 +1,31 @@
 import jwt from "jsonwebtoken"
 import Users from "../models/user.model.js"
 
-/* To prevent connection from deleted account with a JWT still valid */
-const checkUserExists = async(req, res) => {
-    const user = await Users.findById(req.user.id)
-    if (!user) return res.status(401).json({message: "User no longer exists"})
+const checkUserExists = async (userId) => {
+    const user = await Users.findById(userId)
+    return !!user
 }
 
-const validToken = async(req, res, next) => {
+const validToken = async (req, res, next) => {
     const token = req.cookies.token
 
-    if (!token){
-        return res.status(401).json({message: "Unauthentified"})
+    if (!token) {
+        return res.status(401).json({ message: "Unauthentified" })
     }
 
-    try{
+    try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = payload /* payload = { id: "...", role: "coach", iat: ..., exp: ... } */
-        await checkUserExists(req, res)
+        req.user = payload
+
+        const userExists = await checkUserExists(payload.id)
+        if (!userExists) {
+            return res.status(401).json({ message: "User no longer exists" })
+        }
+
         next()
     }
-    catch(err){
-        return res.status(err.status || 401).json({ message: err.message });
+    catch (err) {
+        return res.status(err.status || 401).json({ message: err.message })
     }
 }
 
