e9be06ddb11c4d5df32c2e9064fa73cd2d4f9522
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-22 18:55:19 +0200

    feat: trust proxy

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,9 @@ import responseRouter from './routes/response.route.js'
 
 /* --- CORS --- */
 const app = express()
+
+app.set('trust proxy', 1)
+
 const allowedOrigins = [
     process.env.CLIENT_URL,
     'http://localhost:5173'
