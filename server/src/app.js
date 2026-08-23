daefc72c48e9ff7b4459a1bf1c9ba800586cc997
Guillaume <guillaume.depelchin.etu@univ-lille.fr>
2026-08-23 13:03:16 +0200

    feat: app cors

--- server/src/app.js
+++ server/src/app.js
@@ -22,17 +22,15 @@ const allowedOrigins = [
     'http://localhost:5173'
 ]
 
-app.use(cors({
+const corsOptions = {
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+        const isAllowed = !origin || allowedOrigins.includes(origin)
+        callback(null, isAllowed)
     },
     credentials: true
-}))
+}
 
+app.use(cors(corsOptions))
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 app.use(limiter)
